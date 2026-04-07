ecd3042e28da984456851be8c64ff56517c62730
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-04-07 12:08:29 -0300

    Update exercise_reverse_list.py

--- exercise_reverse_list.py
+++ exercise_reverse_list.py
@@ -1,13 +1,7 @@
 # Ejercicio 8: Invertir una lista
 
 def reverse_list(lista):
-    """
-    Retorna una nueva lista con los elementos en orden inverso.
-
-    Args:
-        lista: Una lista de elementos
-
-    Returns:
-        Una nueva lista con los elementos en orden inverso
-    """
-    pass  # Reemplazar con tu implementación
+    if len(lista) == 0:
+        return []
+    else:
+        return lista[:][::-1]
