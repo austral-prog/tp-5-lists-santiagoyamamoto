d2bed0c2e228e90b01989bb9be47ed3a484b3ab2
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-04-07 12:12:25 -0300

    Update exercise_add_elements.py

--- exercise_add_elements.py
+++ exercise_add_elements.py
@@ -1,7 +1,5 @@
 # Ejercicio 3: Agregar elementos al principio y final
 from mistune.plugins.formatting import insert
-
-
 def add_elements(lista):
     lista.insert(0,'Pink')
     lista.append('Yellow')
